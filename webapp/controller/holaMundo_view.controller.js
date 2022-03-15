faf02e835b0028893daedf988290bca25154b1d1
andresorel <you@example.com>
2022-03-15 12:45:01 -0300

    ajuste

--- webapp/controller/holaMundo_view.controller.js
+++ webapp/controller/holaMundo_view.controller.js
@@ -1,15 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("holamundo.controller.holaMundo_view", {
             onInit: function () {
-
+                MessageBox.confirm("Ejemplo testing");
             }
         });
     });
